ca8286d9b7d7219c99138fc687b9c35e22a591b3
Pratith544 <pratithbhat@gmail.com>
2025-06-22 19:34:33 +0530

    Fixed dependecies

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+
 import ClientOnly from "@/components/ClientOnly";
 import Container from "@/components/Container";
 import EmptyState from "@/components/EmptyState";
@@ -25,15 +27,13 @@ export default async function Home({ searchParams }: HomeProps) {
     <ClientOnly>
       <Container>
         <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 gap-8 overflow-x-hidden">
-          {listing.map((list) => {
-            return (
-              <ListingCard
-                key={list.id}
-                data={list}
-                currentUser={currentUser}
-              />
-            );
-          })}
+          {listing.map((list) => (
+            <ListingCard
+              key={list.id}
+              data={list}
+              currentUser={currentUser}
+            />
+          ))}
         </div>
       </Container>
     </ClientOnly>
